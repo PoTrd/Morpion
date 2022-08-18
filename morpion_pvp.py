dc47d1cb1d0350f2ed0ea299a9b81f726ef48b04
PoTrd <94467775+PoTrd@users.noreply.github.com>
2022-08-18 12:57:55 +0200

    Update morpion_pvp.py
    Ajout d'une fonctionnalité play_again et regroupement dans une fonction morprion_pvp (objectif: créer une autre fonction pour jouer contre un bot)

--- morpion_pvp.py
+++ morpion_pvp.py
@@ -9,50 +9,65 @@ def annonce():
     print("7 | 8 | 9")
 
 
-def affichage():
-    print(plateau[0], "|", plateau[1], "|", plateau[2])
-    print("--+---+--")
-    print(plateau[3], "|", plateau[4], "|", plateau[5])
-    print("--+---+--")
-    print(plateau[6], "|", plateau[7], "|", plateau[8])
+def morpion_pvp():
+    annonce()
+    # Création du plateau qui enregistre les positions
+    plateau = [" " for _ in range(9)]
+    compteur = 0
+    partie_en_cours = True
 
+    def affichage():
+        print(plateau[0], "|", plateau[1], "|", plateau[2])
+        print("--+---+--")
+        print(plateau[3], "|", plateau[4], "|", plateau[5])
+        print("--+---+--")
+        print(plateau[6], "|", plateau[7], "|", plateau[8])
 
-annonce()
-# Création du plateau qui enregistre les positions
-plateau = [" " for _ in range(9)]
-compteur = 0
-partie_en_cours = True
-
-while partie_en_cours == True:
-    position = int(input("Choisis un chiffre entre 1 et 9 : "))
-    position = position - 1  # Une liste commence par 0, 1, 2, ...
-    compteur = compteur + 1
-    if plateau[position] != " ":
-        print("ERROR >> Case déjà prise")
-        # Annule le tour et la ligne suivante lance la nouvelle itération
-        compteur = compteur - 1
-        continue
-    else:
-        if compteur % 2 == 0:  # vérifie si le compteur de tour est paire ou impair pour attribuer des O ou des X
-            plateau[position] = "O"
+    while partie_en_cours == True:
+        position = int(input("Choisis un chiffre entre 1 et 9 : "))
+        position = position - 1  # Une liste commence par 0, 1, 2, ...
+        compteur = compteur + 1
+        if plateau[position] != " ":
+            print("ERROR >> Case déjà prise")
+            # Annule le tour et la ligne suivante lance la nouvelle itération
+            compteur = compteur - 1
+            continue
         else:
-            plateau[position] = "X"
+            if compteur % 2 == 0:  # vérifie si le compteur de tour est paire ou impair pour attribuer des O ou des X
+                plateau[position] = "O"
+            else:
+                plateau[position] = "X"
 
-    affichage()
+        affichage()
 
-# Impossible de créer une fonction rules et d'y inclure break pour stoper la partie une fois remportée
-    if plateau[0] == plateau[1] == plateau[2] \
-            or plateau[3] == plateau[4] == plateau[5] != " "\
-            or plateau[6] == plateau[7] == plateau[8] != " "\
-            or plateau[0] == plateau[3] == plateau[6] != " "\
-            or plateau[1] == plateau[4] == plateau[7] != " "\
-            or plateau[2] == plateau[5] == plateau[8] != " "\
-            or plateau[0] == plateau[4] == plateau[8] != " "\
-            or plateau[6] == plateau[4] == plateau[2] != " ":
-        print("Bravo, les {0} gagnent !" .format(plateau[position]))
-        break
-    elif compteur == 9:
-        print("Egalité !")
-        break
+    # Impossible de créer une fonction rules et d'y inclure break pour stoper la partie une fois remportée
+        if plateau[0] == plateau[1] == plateau[2] != " " \
+                or plateau[3] == plateau[4] == plateau[5] != " "\
+                or plateau[6] == plateau[7] == plateau[8] != " "\
+                or plateau[0] == plateau[3] == plateau[6] != " "\
+                or plateau[1] == plateau[4] == plateau[7] != " "\
+                or plateau[2] == plateau[5] == plateau[8] != " "\
+                or plateau[0] == plateau[4] == plateau[8] != " "\
+                or plateau[6] == plateau[4] == plateau[2] != " ":
+            print("Bravo, les {0} gagnent !" .format(plateau[position]))
+            play_again()
+            break
+
+        elif compteur == 9:
+            print("Egalité !")
+            play_again()
+            break
+
+        else:
+            partie_en_cours = True
+
+
+def play_again():
+    play = input("Voulez vous rejouer ? (Y/N)")
+    if play == "y" or play == "Y" or play == "YES" or play == "yes" or play == "Yes":
+        morpion_pvp()
     else:
-        partie_en_cours = True
+        exit
+
+
+morpion_pvp()
